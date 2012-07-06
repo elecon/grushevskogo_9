16d3e982001385cfd5d219d4887af786a16ce650
zz <zoloto@elecon.kiev.ua>
2012-07-06 15:26:56 +0300

    Add new files for KPD

---
+++ Devs/Dev_kpd.cpp
@@ -0,0 +1,445 @@
+#include "Dev_kog.h"
+	void KOG::auto_open()
+	{
+        stan |= KOG_OP;
+        stan &=~ KOG_CLOSE;
+	}
+	void KOG::auto_close()
+	{
+		stan &=~ KOG_OP;
+        stan |= KOG_CLOSE;
+	}
+	void KOG::check_press()
+	{
+		if (stan&KOG_OP) {
+            if (*k_press) t_kog_wait=T_WAIT_PRESS;
+        }
+        if (stan&KOG_CLOSE) {
+            if (!(*k_press)) t_kog_wait=T_WAIT_PRESS;
+        }
+        if(!sec_tik) if(t_kog_wait) t_kog_wait--;
+        if(!(t_kog_wait)) stan|=KOG_STAN;
+	}
+
+	void KOG::check_mp()
+	{
+		if (stan&KOG_OP) {
+            if (!(*k_mp)) t_kog_wait=T_WAIT_PRESS;
+        }
+        if (stan&KOG_CLOSE) {
+            if ((*k_mp)) t_kog_wait=T_WAIT_PRESS;
+        }
+        if(!sec_tik) if(t_kog_wait) t_kog_wait--;
+        if(!(t_kog_wait)) stan|=KOG_STAN;
+	}
+
+	void KOG::check_os()
+	{
+		if(!(*os)) stan|=KOG_OS_ERROR;
+        else stan&=~KOG_OS_ERROR;
+	}
+
+	void KOG::drv()
+	{
+		if (stan&(KOG_OPER|KOG_DIST)) {
+        if (stad&(1<<KOG_ST_OPEN)) {
+            stad&=~((1<<KOG_ST_OPEN));
+            stan |= KOG_OP;
+            stan &=~ KOG_CLOSE;
+            //OUT_FIELD[OUT_BIT]=0xff;
+        }
+        if (stad&(1<<KOG_ST_CLOSE)) {
+            stad&=~(1<<KOG_ST_CLOSE);
+            stan &=~ KOG_OP;
+            stan |= KOG_CLOSE;
+            //OUT_FIELD[OUT_BIT]=0x00;
+        }
+
+        if (stad&(1<<KOG_ST_AVTO)) {
+            stad&=~(1<<KOG_ST_AVTO);
+            stan&=~(KOG_OPER|KOG_DIST);
+        }
+
+        if (stad&(1<<KOG_ST_ERR)) {
+            stad&=~(1<<KOG_ST_ERR);
+            stan&=~KOG_STAN;
+        }
+    }
+    else {
+        if (stad&(1<<KOG_ST_OPER)) {
+            stad&=~(1<<KOG_ST_OPER);
+            stan|=KOG_OPER;
+        }
+        else {
+            if (stad&(1<<KOG_ST_DIST)) {
+                stad&=~(1<<KOG_ST_DIST);
+                stan|=KOG_DIST;
+            }
+        }
+    }
+    if (stan&KOG_CLOSE) *out=0x00;
+    else if (stan&KOG_OP) *out=0xff;
+
+    if(enh_info&OPT_CHECK_PRESS) check_press();
+    if(enh_info&OPT_CHECK_MP) check_mp();
+    if(enh_info&OPT_CHECK_OS) check_os();
+	}
+
+	KOG::KOG()
+	{
+		cmd = KOG_C_CLOSE;
+		t_wait_press=10;
+		t_wait_mp=5;
+		t_wait_press_counter=t_wait_press;
+		t_wait_mp_counter=t_wait_mp;
+
+		t_kog_wait=50;
+        stan |= KOG_CLOSE;
+	}
+
+	void KOG::show_mnem()
+	{
+        if (stan&KOG_OP) *mn_os=green_lamp;
+        else *mn_os=null_lamp;
+
+        if (stan&KOG_STAN) *mn_os&=~0x11;
+        if (stan&KOG_OS_ERROR) *mn_os&=~0x11;
+        if (stan&(KOG_OPER|KOG_DIST)) *mn_os&=~0x11;
+
+		//*mn_os = lamp;
+	}
+
+	void KOG::show_lcd()
+	{
+        LCD_abc((char*)name,0);
+
+        if  (stan&KOG_OP) LCD_abc((char*)stad_str[0],6);
+        else LCD_abc((char*)stad_str[1],6);
+        if (stad&(1<<KOG_C_OPEN)) LCD_abc((char*)stad_str[0],4);
+        if (stad&(1<<KOG_C_CLOSE)) LCD_abc((char*)stad_str[1],4);
+
+        if (stan&(KOG_OPER|KOG_DIST)) {
+            if (stan&KOG_DIST) LCD_abc((char*)dist_str,   12);//19
+            if (stan&KOG_OPER) LCD_abc((char*)oper_str,   12);//19
+        }
+        else {
+            LCD_abc((char*)auto_str,   12);//19
+        }
+
+        if (stan&(KOG_OS_ERROR|KOG_STAN)) {
+            LCD_abc((char*)error_str,20);
+            if (stan&KOG_OS_ERROR)     LCD_abc((char*)os_err_str,      34);
+            if (stan&KOG_STAN)   LCD_abc((char*)press_err_str,   23);//19
+        }
+//        LCD_uind(stad,28,2|LEAD_ZERO);
+//        LCD_uind(stan,31,2|LEAD_ZERO);
+//        LCD_uind(t_kog_wait,33,2|LEAD_ZERO);
+
+        if (stan&KOG_OPER) {
+            if(KEY_8) {
+                if(!(stad&(1<<KOG_ST_OPEN)))
+                {
+                    stad|=(1<<KOG_ST_OPEN);
+                }
+            }
+            if(KEY_NO) {
+                if(!(stad&(1<<KOG_ST_CLOSE)))
+                {
+                    stad|=(1<<KOG_ST_CLOSE);
+                }
+            }
+            if (KEY_1) stad|=(1<<KOG_ST_ERR);
+            if (KEY_4) {
+                stad|=(1<<KOG_ST_AVTO);
+            }
+        }
+        else {
+            if (!(stan&(KOG_OPER|KOG_DIST))) if (KEY_7) stad|=(1<<KOG_ST_OPER);
+            if (stan&(KOG_DIST|KOG_OPER)) if (KEY_4) stad|=(1<<KOG_ST_AVTO);
+        }
+	}
+
+	KOG::KOG(	const prog_char* _name,
+				uint8_t* _mn_os,
+				uint16_t _enh_info,
+				uint8_t* _out,
+				uint8_t* _press,
+				uint8_t* _os,
+				uint8_t* _mp,
+				uint8_t* _ktr)
+	{
+//		clear_t_stad();
+//		clear_t_sub_st();
+		cmd = KOG_C_CLOSE;
+		t_wait_press=10;
+		t_wait_mp=5;
+		t_wait_press_counter=t_wait_press;
+		t_wait_mp_counter=t_wait_mp;
+		name=_name;
+		mn_os=_mn_os;
+		enh_info=_enh_info;
+		out=_out;
+		k_press=_press;
+		os=_os;
+		k_mp=_mp;
+		k_ktr=_ktr;
+	}
+//______________________________________________________________________________
+	void KogNum::auto_open()
+	{
+        stan |= KOG_OP;
+        stan &=~ KOG_CLOSE;
+	}
+	void KogNum::auto_close()
+	{
+		stan &=~ KOG_OP;
+        stan |= KOG_CLOSE;
+	}
+	void KogNum::check_press()
+	{
+		if (stan&KOG_OP) {
+            if (OS_FIELD[Dev_constr[num][3]]) t_kog_wait=T_WAIT_PRESS;
+        }
+        if (stan&KOG_CLOSE) {
+            if (!(OS_FIELD[Dev_constr[num][3]])) t_kog_wait=T_WAIT_PRESS;
+        }
+        if(!sec_tik) if(t_kog_wait) t_kog_wait--;
+        if(!(t_kog_wait)) stan|=KOG_STAN;
+	}
+
+	void KogNum::check_mp()
+	{
+		if (stan&KOG_OP) {
+            if (!(MSK_FIELD[Dev_constr[num][4]])) t_kog_wait=T_WAIT_PRESS;
+        }
+        if (stan&KOG_CLOSE) {
+            if ((MSK_FIELD[Dev_constr[num][4]])) t_kog_wait=T_WAIT_PRESS;
+        }
+        if(!sec_tik) if(t_kog_wait) t_kog_wait--;
+        if(!(t_kog_wait)) stan|=KOG_STAN;
+	}
+
+	void KogNum::check_os()
+	{
+		if(!(OS_FIELD[Dev_constr[num][1]])) stan|=KOG_OS_ERROR;
+        else stan&=~KOG_OS_ERROR;
+	}
+
+	void KogNum::drv()
+	{
+		if (stan&(KOG_OPER|KOG_DIST)) {
+        if (stad&(1<<KOG_ST_OPEN)) {
+            stad&=~((1<<KOG_ST_OPEN));
+            stan |= KOG_OP;
+            stan &=~ KOG_CLOSE;
+            //OUT_FIELD[OUT_BIT]=0xff;
+        }
+        if (stad&(1<<KOG_ST_CLOSE)) {
+            stad&=~(1<<KOG_ST_CLOSE);
+            stan &=~ KOG_OP;
+            stan |= KOG_CLOSE;
+            //OUT_FIELD[OUT_BIT]=0x00;
+        }
+
+        if (stad&(1<<KOG_ST_AVTO)) {
+            stad&=~(1<<KOG_ST_AVTO);
+            stan&=~(KOG_OPER|KOG_DIST);
+        }
+
+        if (stad&(1<<KOG_ST_ERR)) {
+            stad&=~(1<<KOG_ST_ERR);
+            stan&=~KOG_STAN;
+        }
+    }
+    else {
+        if (stad&(1<<KOG_ST_OPER)) {
+            stad&=~(1<<KOG_ST_OPER);
+            stan|=KOG_OPER;
+        }
+        else {
+            if (stad&(1<<KOG_ST_DIST)) {
+                stad&=~(1<<KOG_ST_DIST);
+                stan|=KOG_DIST;
+            }
+        }
+    }
+    if (stan&KOG_CLOSE) OUT_FIELD[Dev_constr[num][1]]=0x00;
+    else if (stan&KOG_OP) OUT_FIELD[Dev_constr[num][1]]=0xff;
+
+    if(Dev_constr_info[num]&OPT_CHECK_PRESS) check_press();
+    if(Dev_constr_info[num]&OPT_CHECK_MP) check_mp();
+    if(Dev_constr_info[num]&OPT_CHECK_OS) check_os();
+	}
+
+	KogNum::KogNum()
+	{
+
+		t_wait_press=10;
+		t_wait_mp=5;
+		t_wait_press_counter=t_wait_press;
+		t_wait_mp_counter=t_wait_mp;
+
+		t_kog_wait=50;
+        stan |= KOG_CLOSE;
+	}
+
+	void KogNum::show_mnem()
+	{
+        if (stan&KOG_OP) LED_FIELD[Dev_constr[num][0]]=green_lamp;
+        else LED_FIELD[Dev_constr[num][0]]=null_lamp;
+
+        if (stan&KOG_STAN) LED_FIELD[Dev_constr[num][0]]&=~0x11;
+        if (stan&KOG_OS_ERROR) LED_FIELD[Dev_constr[num][0]]&=~0x11;
+        if (stan&(KOG_OPER|KOG_DIST)) LED_FIELD[Dev_constr[num][0]]&=~0x11;
+
+		//*mn_os = lamp;
+	}
+
+	void KogNum::show_lcd()
+	{
+        LCD_abc((char*)dev_name_str[num],0);
+
+        if  (stan&KOG_OP) LCD_abc((char*)stad_str[0],6);
+        else LCD_abc((char*)stad_str[1],6);
+        if (stad&(1<<KOG_C_OPEN)) LCD_abc((char*)stad_str[0],4);
+        if (stad&(1<<KOG_C_CLOSE)) LCD_abc((char*)stad_str[1],4);
+
+        if (stan&(KOG_OPER|KOG_DIST)) {
+            if (stan&KOG_DIST) LCD_abc((char*)dist_str,   12);//19
+            if (stan&KOG_OPER) LCD_abc((char*)oper_str,   12);//19
+        }
+        else {
+            LCD_abc((char*)auto_str,   12);//19
+        }
+
+        if (stan&(KOG_OS_ERROR|KOG_STAN)) {
+            LCD_abc((char*)error_str,20);
+            if (stan&KOG_OS_ERROR)     LCD_abc((char*)os_err_str,      34);
+            if (stan&KOG_STAN)   LCD_abc((char*)press_err_str,   23);//19
+        }
+//        LCD_uind(stad,28,2|LEAD_ZERO);
+//        LCD_uind(stan,31,2|LEAD_ZERO);
+//        LCD_uind(t_kog_wait,33,2|LEAD_ZERO);
+
+        if (stan&KOG_OPER) {
+            if(KEY_8) {
+                if(!(stad&(1<<KOG_ST_OPEN)))
+                {
+                    stad|=(1<<KOG_ST_OPEN);
+                }
+            }
+            if(KEY_NO) {
+                if(!(stad&(1<<KOG_ST_CLOSE)))
+                {
+                    stad|=(1<<KOG_ST_CLOSE);
+                }
+            }
+            if (KEY_1) stad|=(1<<KOG_ST_ERR);
+            if (KEY_4) {
+                stad|=(1<<KOG_ST_AVTO);
+            }
+        }
+        else {
+            if (!(stan&(KOG_OPER|KOG_DIST))) if (KEY_7) stad|=(1<<KOG_ST_OPER);
+            if (stan&(KOG_DIST|KOG_OPER)) if (KEY_4) stad|=(1<<KOG_ST_AVTO);
+        }
+	}
+
+	KogNum::KogNum(uint8_t _num)
+	{
+        num=_num;
+	}
+//______________________________________________________________________________
+	void KOG_NET::drv()
+	{
+        stan = *net_stan;
+        if (stan&KOG_OP) stad&=~(1<<KOG_ST_OPEN);
+        if (stan&KOG_CLOSE) stad&=~(1<<KOG_ST_CLOSE);
+        if (!(stan&(KOG_OPER|KOG_DIST))) stad&=~(1<<KOG_ST_AVTO);
+        if (stan&KOG_OPER) stad&=~(1<<KOG_ST_OPER);
+        *net_stad = stad;
+	}
+
+	void KOG_NET::show_mnem()
+	{
+		if (stan&KOG_OP) *mn_os =green_lamp;
+        else *mn_os =null_lamp;
+
+        if (stan&KOG_STAN) *mn_os&=~0x11;
+        if (stan&KOG_OS_ERROR) *mn_os&=~0x11;
+        if (stan&(KOG_OPER|KOG_DIST)) *mn_os&=~0x11;
+
+
+	}
+
+	void KOG_NET::show_lcd()
+	{
+        LCD_abc((char*)name,0);
+        if (net_fl&(1<<num)) {
+            LCD_abc((char*)error_str,6);
+        }
+        else {
+            if  (stan&KOG_OP) LCD_abc((char*)stad_str[0],6);
+            else LCD_abc((char*)stad_str[1],6);
+            if (stad&(1<<KOG_C_OPEN)) LCD_abc((char*)stad_str[0],4);
+            if (stad&(1<<KOG_C_CLOSE)) LCD_abc((char*)stad_str[1],4);
+
+            if (stan&(KOG_OPER|KOG_DIST)) {
+                if (stan&KOG_DIST) LCD_abc((char*)dist_str,   12);//19
+                if (stan&KOG_OPER) LCD_abc((char*)oper_str,   12);//19
+            }
+            else {
+                LCD_abc((char*)auto_str,   12);//19
+            }
+
+            if (stan&(KOG_OS_ERROR|KOG_STAN)) {
+                LCD_abc((char*)error_str,20);
+                if (stan&KOG_OS_ERROR) LCD_abc((char*)os_err_str,      34);
+                if (stan&KOG_STAN)   LCD_abc((char*)press_err_str,   23);//19
+            }
+
+            //LCD_uind(stad,28,2|LEAD_ZERO);
+            //LCD_uind(stan,31,2|LEAD_ZERO);
+
+            if (stan&KOG_OPER) {
+                if(KEY_8) {
+                    if(!(stad&(1<<KOG_ST_OPEN)))
+                    {
+                        stad|=(1<<KOG_ST_OPEN);
+                    }
+                }
+                if(KEY_NO) {
+                    if(!(stad&(1<<KOG_ST_CLOSE)))
+                    {
+                        stad|=(1<<KOG_ST_CLOSE);
+                    }
+                }
+                if (KEY_1) stad|=(1<<KOG_ST_ERR);
+                if (KEY_4) {
+                    stad|=(1<<KOG_ST_AVTO);
+                }
+            }
+            else {
+                if (!(stan&(KOG_OPER|KOG_DIST))) if (KEY_7) stad|=(1<<KOG_ST_OPER);
+                if (stan&(KOG_DIST|KOG_OPER)) if (KEY_4) stad|=(1<<KOG_ST_AVTO);
+            }
+        }
+    }
+    KOG_NET::KOG_NET()
+    {
+
+    }
+
+	KOG_NET::KOG_NET
+	(const prog_char* _name,
+        uint8_t* _net_stan,
+        uint8_t* _net_stad,
+        uint8_t* _mn_os,
+        uint8_t _num)
+        {
+            name=_name;
+            net_stad=_net_stad;
+            net_stan=_net_stan;
+            mn_os=_mn_os;
+            num=_num;
+        }
---
+++ Devs/Dev_kpd.h
@@ -0,0 +1,134 @@
+#ifndef __DEVKOG_H
+	#define __DEVKOG_H
+
+	#include "DEVS.H"
+    #include "Nls.h"
+
+    #define T_WAIT_PRESS 5
+    #define T_WAIT_MP 2
+
+    #define PAUSE 5
+    #define WORK 2
+
+	enum {KOG_C_CLOSE,KOG_C_OPEN};
+	//enum {KOGTT_C_CLOSE,KOGTT_C_OPEN};
+	enum {  KOG_ST_CLOSE,   //0x01
+            KOG_ST_OPEN,    //0x02
+            KOG_ST_OPER,    //0x04
+            KOG_ST_DIST,    //0x08
+            KOG_ST_AVTO,    //0x10
+
+            KOG_ST_ERR      //0x20
+    };
+
+	//флаги enh_info
+//	#define KOG_CHECK_PRESS	0x8000 //проверка перепада давления
+//	#define KOG_CHECK_MP	0x4000 //проверка состояния магнитного пускателя
+//	#define KOG_CHECK_KTR	0x2000 //проверка состояния теплового реле
+//	#define KOG_CHECK_OS	0x1000 //проверка обратной связи
+//	#define KOG_OPER		0x0200 //устройство управляется оператором
+//	#define KOG_DIST		0x0100 //устройство управляется дистанционно
+//	#define KOG_PRESS_ERROR	0x0080 //ошибка перепада давления
+//	#define KOG_MP_ERROR	0x0040 //ошибка магнитного пускателя
+//	#define KOG_KTR_ERROR	0x0020 //ошибка теплового реле
+//	#define KOG_OS_ERROR	0x0010 //ошибка обратной связи
+
+    #define KOG_AUTO    	0x01 //
+	#define KOG_OPER    	0x02 //
+	#define KOG_DIST    	0x04 //
+	#define KOG_OP      	0x08 //
+	#define KOG_SUXAR   	0x80 //ошибка перепада давления
+	#define KOG_CLOSE   	0x40 //ошибка магнитного пускателя
+	#define KOG_STAN    	0x20 //ошибка теплового реле
+	#define KOG_OS_ERROR	0x10 //ошибка обратной связи
+
+	class KOG: public TDevObject
+	{
+		public:
+			uint8_t *out;//выбор выхода
+			uint8_t *k_press;
+			uint8_t *os;//указатель на байт обратной связи
+			uint8_t *k_mp;
+			uint8_t *k_ktr;
+			uint16_t enh_info;
+			uint8_t *mn_os;
+			int8_t s_n;
+			uint8_t cmd;
+			const prog_char *name;
+
+
+			uint8_t t_repit_counter;
+            uint8_t t_kog_wait;
+
+			uint16_t t_wait_press;
+			uint16_t t_wait_press_counter;
+			uint16_t t_wait_mp;
+			uint16_t t_wait_mp_counter;
+			void drv();
+			void show_mnem();
+			void show_lcd();
+			void check_press();
+			void check_mp();
+			void check_ktr();
+			void check_os();
+			void auto_open();
+			void auto_close();
+			KOG();
+			KOG(	const prog_char* _name,
+					uint8_t* _mn_os,
+					uint16_t enh_info,
+					uint8_t* _out,
+					uint8_t* _press,
+					uint8_t* _os,
+					uint8_t* _mp,
+					uint8_t* _ktr);
+	};
+
+	class KogNum: public TDevObject
+	{
+		public:
+			uint16_t enh_info;
+			uint8_t num;
+
+
+			uint8_t t_repit_counter;
+            uint8_t t_kog_wait;
+
+			uint16_t t_wait_press;
+			uint16_t t_wait_press_counter;
+			uint16_t t_wait_mp;
+			uint16_t t_wait_mp_counter;
+			void drv();
+			void show_mnem();
+			void show_lcd();
+			void check_press();
+			void check_mp();
+			void check_ktr();
+			void check_os();
+			void auto_open();
+			void auto_close();
+			KogNum();
+			KogNum(uint8_t _num);
+	};
+
+    class KOG_NET: public TDevObject
+	{
+		public:
+            uint8_t *net_stan;
+            uint8_t *net_stad;
+            uint8_t *mn_os;
+            uint8_t num;
+			const prog_char *name;
+			void drv();
+			void show_mnem();
+			void show_lcd();
+
+			KOG_NET();
+			KOG_NET (const prog_char* _name,
+					uint8_t* _net_stan,
+					uint8_t* _net_stad,
+					uint8_t* _mn_os,
+					uint8_t _num);
+	};
+
+#endif
